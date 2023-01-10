54091a6187ec76b1b80449649506cca58eaf563a
apriori85 <victor-timoshin@hotmail.com>
2023-01-10 10:18:23 +0300

    Added sway.module_loader submodule

--- lib/include/sway/loader/png/pngplugin.hpp
+++ lib/include/sway/loader/png/pngplugin.hpp
@@ -2,36 +2,19 @@
 #define SWAY_LOADER_PNG_PNGPLUGIN_HPP
 
 #include <sway/core.hpp>
-
-#define LOADER_PLUGIN_EXPORT  // empty
-
-#define DECLARE_LOADER_PLUGIN(plugclass, plugname, version)                                                   \
-  extern "C" {                                                                                                \
-  LOADER_PLUGIN_EXPORT sway::loader::png::LoaderPlugin *getInstance() {                                       \
-    static plugclass singleton;                                                                               \
-    return &singleton;                                                                                        \
-  }                                                                                                           \
-  LOADER_PLUGIN_EXPORT sway::core::PluginInfo pluginGetInfo() { return {plugname, "", "", "", "", version}; } \
-  LOADER_PLUGIN_EXPORT sway::loader::png::LoaderPluginInfo getExports() { return {getInstance}; }             \
-  }
+#include <sway/loader.hpp>
 
 NAMESPACE_BEGIN(sway)
 NAMESPACE_BEGIN(loader)
 NAMESPACE_BEGIN(png)
 
-class LoaderPlugin {};
-
-typedef LoaderPlugin *(*LoaderPluginInitializeFunc_t)();
-
-struct LoaderPluginInfo {
-  LoaderPluginInitializeFunc_t plug;
-};
-
-class PNGPlugin : public LoaderPlugin {
+class PNGPlugin : public ImageLoaderPlugin {
 public:
   PNGPlugin();
 
   virtual ~PNGPlugin() = default;
+
+  [[nodiscard]] virtual std::string getText() const { return "Hello"; }
 };
 
 NAMESPACE_END(png)
